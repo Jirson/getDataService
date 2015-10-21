a3afe76127d827f3c79cef7fa8c85a755c067a70
Jirson Tavera <JirsonTavera@hotmail.com>
2015-10-20 22:48:38 -0500

    Bostrap and class

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Angularjs getting Datas</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
     <script type="text/javascript" src="app.js"></script>
 </head>
@@ -9,10 +10,10 @@
     <div ng-app="miApp">
         <div ng-controller="miController">
             <input type="text" name="search" ng-model="search" autofocus>
-            <input type="button" value="Get Datas!" ng-click="getDatas()">
+            <input type="button" value="Get Datas!" ng-click="getDatas()" class="btn btn-default">
             <p>Busco por termino: <span> {{search}}</span></p>
-            <div>Datas results
-            <span>{{details.Title}}</span>, {{details.Year}}
+            <div>Datas results<br>
+                <span class="span-outer"><a href="">{{details.Title}}</a></span>, {{details.Year}}
             </div>
         </div>
     </div>
